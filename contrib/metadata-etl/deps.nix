d3a48bed0913c58c53ffde76eb8363bcfbcfc0f9
clojurians-org <larluo@clojurians.org>
2020-01-14 23:21:13 +0800

    init queryparser dependency

--- contrib/metadata-etl/deps.nix
+++ contrib/metadata-etl/deps.nix
@@ -1,8 +1,22 @@
 with import <nixos-19.09> {} ;
+let 
+  haskellPackages = pkgs.haskellPackages.override {
+    overrides = self: super: with pkgs.haskell.lib; {
+      queryparser = doJailbreak(self.callCabal2nix "queryparser" (fetchFromGitHub {
+        owner = "uber" ;
+        repo = "queryparser" ;
+        rev = "6015e8f273f4498326fec0315ac5580d7036f8a4" ;
+        sha256 = "05pnifm5awyqxi6330v791b1cvw26xbcn2r20pqakvl8d3xyaxa4" ;
+      }) {}) ;
+
+    } ;
+  };
+in 
 mkShell {
   buildInputs = [
-    (haskellPackages.ghcWithPackages ( p: with p ;
-      [ text lens hssqlppp ]
+    (haskellPackages.ghcWithPackages ( p: 
+      #[ p.text p.lens p.hssqlppp p.queryparser ]
+      [ p.text p.lens p.hssqlppp ]
     ))
   ];
 }
