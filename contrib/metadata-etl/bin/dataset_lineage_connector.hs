031fa35bbda99844d51be5d57c0cc92b793ca04a
clojurians-org <larluo@clojurians.org>
2020-01-14 21:47:46 +0800

    add pg example for test

--- contrib/metadata-etl/bin/dataset_lineage_connector.hs
+++ contrib/metadata-etl/bin/dataset_lineage_connector.hs
@@ -1,7 +1,12 @@
 #! /usr/bin/env nix-shell
 #! nix-shell ../deps.nix -i runghc
 
+import qualified Data.Text.Lazy.IO as T
+import Database.HsSqlPpp.Parse (parseProcSQL, defaultParseFlags, ParseFlags(..))
+import Database.HsSqlPpp.Dialect (postgresDialect)
 import Control.Lens
-main = do
-  putStrLn $ ("hello","world")^._2
+main = do 
+  content <-  T.readFile "./metadata_sample/pg_procedure.sql"
+  let ast = parseProcSQL (ParseFlags {pfDialect = postgresDialect}) "-" Nothing content
+  print $ ast
 
--- contrib/metadata-etl/deps.nix
+++ contrib/metadata-etl/deps.nix
@@ -2,7 +2,7 @@ with import <nixos-19.09> {} ;
 mkShell {
   buildInputs = [
     (haskellPackages.ghcWithPackages ( p: with p ;
-      [ lens hssqlppp ]
+      [ text lens hssqlppp ]
     ))
   ];
 }
